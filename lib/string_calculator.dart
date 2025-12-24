f0a5704c312ac4203d8fc3e54248af32f0d0b6b7
Abhishek Jain <abhishek.jain@yocket.in>
2025-12-24 12:07:10 +0530

    should support single-character custom delimiters [Pass]

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,8 +1,15 @@
 class StringCalculator{
   int add(String number){
+    String numbersPart = number;
+    Pattern delimiterPattern = RegExp(',|\n');
     if(number.isEmpty) return 0;
-    final delimiterRegex = RegExp(',|\n');
-    final parts = number.split(delimiterRegex);
+    if (numbersPart.startsWith('//')) {
+      final newLineIndex = number.indexOf('\n');
+      final delimiter = number.substring(2, newLineIndex);
+      delimiterPattern = RegExp(RegExp.escape(delimiter));
+      numbersPart = number.substring(newLineIndex + 1);
+    }
+    final parts = numbersPart.split(delimiterPattern);
     return parts.map(int.parse).fold(0, (sum,current)=>sum+current);
   }
 }
