beb707905f01bde61095ff2df842fa320d2c3d95
Abhishek Jain <abhishek.jain@yocket.in>
2025-12-24 12:31:26 +0530

    feat: some error handling added

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,20 +1,22 @@
-class StringCalculator{
-  int add(String number){
+class StringCalculator {
+  int add(String number) {
     String numbersPart = number;
     Pattern delimiterPattern = RegExp(',|\n');
-    if(number.isEmpty) return 0;
+    if (number.isEmpty) return 0;
     if (numbersPart.startsWith('//')) {
       final newLineIndex = number.indexOf('\n');
-      final delimiter = number.substring(2, newLineIndex);
-      delimiterPattern = RegExp(RegExp.escape(delimiter));
-      numbersPart = number.substring(newLineIndex + 1);
+      if (newLineIndex != -1) {
+        final delimiter = number.substring(2, newLineIndex);
+        delimiterPattern = RegExp(RegExp.escape(delimiter));
+        numbersPart = number.substring(newLineIndex + 1);
+      }
     }
     final parts = numbersPart.split(delimiterPattern);
     final negatives = <int>[];
     int sum = 0;
 
     for (final part in parts) {
-      final value = int.parse(part);
+      final value = int.tryParse(part) ?? 0;
       if (value < 0) {
         negatives.add(value);
       } else {
