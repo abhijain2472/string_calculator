9fa9d3b09ac3e16f3574e07ff155c99b7d560980
Abhishek Jain <abhishek.jain@yocket.in>
2025-12-24 12:35:33 +0530

    feat: sample code added in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
-void main(){
+import 'package:string_calculator/string_calculator.dart';
 
+void main(){
+  final calc = StringCalculator();
+  final result = calc.add('1,2,3');
+  print('result: $result');
 }
