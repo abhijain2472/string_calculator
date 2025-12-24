0a050e1465d46b9f02ed5ae534b6ac4f68de5aac
Abhishek Jain <abhishek.jain@yocket.in>
2025-12-24 11:25:39 +0530

    test: support two comma separated numbers [Pass]

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,6 @@
 class StringCalculator{
   int add(String number){
     if(number.isEmpty) return 0;
-    return int.parse(number);
+    return number.split(',').map(int.parse).fold(0, (sum,current)=>sum+current);
   }
 }
